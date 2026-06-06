fa20c02074dd1b11daf600bb43ed874a6638f25b
John099x <johnbr556@gmail.com>
2026-06-05 23:06:10 -0300

    link pra home do site

--- Login/login.php
+++ Login/login.php
@@ -25,7 +25,7 @@
                 <p id="popup-mensagem"></p>
             </div>
             <button onclick="fecharPopup()">Fechar</button>
-            <a id="popup-link" href="#menu" style="display:none;">Voltar ao menu?</a>
+            <a id="popup-link" href="https://total-loss.freepage.cc/?i=1" style="display:none;">Voltar ao menu?</a>
         </div>
     </div>
     <script src="login.js"></script>
